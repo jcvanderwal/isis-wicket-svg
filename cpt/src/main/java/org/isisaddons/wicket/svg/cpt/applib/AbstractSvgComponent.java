a53530169cd730f9d57391af371385ca8bd0ae95
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2014-11-10 22:54:09 +0200

    Do not keep a second reference to the model instance. It is already available by #getDefaultModel()

--- cpt/src/main/java/org/isisaddons/wicket/svg/cpt/applib/AbstractSvgComponent.java
+++ cpt/src/main/java/org/isisaddons/wicket/svg/cpt/applib/AbstractSvgComponent.java
@@ -1,14 +1,13 @@
 package org.isisaddons.wicket.svg.cpt.applib;
 
+import org.apache.wicket.IGenericComponent;
 import org.apache.wicket.markup.ComponentTag;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.model.IModel;
 
-public abstract class AbstractSvgComponent<T> extends WebMarkupContainer {
+public abstract class AbstractSvgComponent<T> extends WebMarkupContainer implements IGenericComponent<T> {
 
     private static final long serialVersionUID = 1L;
-
-    private IModel<T> model;
 
     public AbstractSvgComponent(final String wicketId) {
         super(wicketId);
@@ -16,7 +15,6 @@ public abstract class AbstractSvgComponent<T> extends WebMarkupContainer {
 
     public AbstractSvgComponent(final String wicketId, final IModel<T> model) {
         super(wicketId, model);
-        this.model = model;
     }
 
     @Override
@@ -30,20 +28,24 @@ public abstract class AbstractSvgComponent<T> extends WebMarkupContainer {
 
     protected abstract String getTag();
 
+    @Override
     public IModel<T> getModel() {
-        return model;
+        return (IModel<T>) getDefaultModel();
     }
 
+    @Override
     public void setModel(final IModel<T> model) {
-        this.model = model;
+        setDefaultModel(model);
     }
 
+    @Override
     public T getModelObject() {
-        return model.getObject();
+        return getModel().getObject();
     }
 
+    @Override
     public void setModelObject(final T object) {
-        model.setObject(object);
+        getModel().setObject(object);
     }
 
 }
